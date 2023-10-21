281154a62c0627af0caf45832fbd0e0aaef947eb
ufy <ufuomaitwa@gmail.com>
2023-10-21 14:58:55 +0100

    edited js files

--- index.js
+++ index.js
@@ -133,10 +133,66 @@ function logIn(event) {
 
 // Dashboard on load
 function dashboardApi() {
+    const getModal = document.querySelector('.pagemodal');
+    getModal.style.display= "block";
+    
     const adminId = document.getElementById("adminId");
     let adminDetails = localStorage.getItem("admin");
     adminDetails = JSON.parse(adminDetails);
     adminId.innerText = `Hello ${adminDetails.name}!`;
+
+    //Get dashboard details
+    
+    const bearerToken = adminDetails.token;
+
+    const url = "https://pluralcodesandbox.com/yorubalearning/api/admin/admin_dashboardapi"; 
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const getCategory = document.getElementById('category');
+        const getLearnmat = document.getElementById('learnmat');
+        const getSubcat = document.getElementById('subCat');
+        const getQuiz = document.getElementById('quiz');
+        const getStudents = document.getElementById('student');
+
+        getCategory.innerText = data['total_number_of_categories'
+        ];
+        getLearnmat.innerText = data['total_number_of_learningmaterial'
+        ];
+        getSubcat.innerText = data['total_number_of_subcategories'
+        ];
+        getQuiz.innerText = data['total_number_of_quize'
+        ];
+        getStudents.innerText = data['total_number_of_students'
+        ];
+    })
+    .catch(error => console.error('Error:', error));
+    
+    // getModal.style.display= "none";
+
+        //Get top 3 students
+    const url1 = "https://pluralcodesandbox.com/yorubalearning/api/admin/top_three_students"; 
+
+    fetch(url1, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const topThree = localStorage.setItem("tpThree",JSON.stringify(data));
+    })
+    .catch(error => console.error('Error:', error));
+
 }
 
 
@@ -184,6 +240,22 @@ function closeDashModal() {
     const studentModal = document.querySelector('.mymodal');
     studentModal.style.display= "none";
 }
+
+//get all students
+const url2 = "https://pluralcodesandbox.com/yorubalearning/api/admin/get_all_students"; 
+
+fetch(url2, {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${bearerToken}`,
+        'Content-Type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    const allStudents = localStorage.setItem("allStudents",JSON.stringify(data));
+})
+.catch(error => console.error('Error::', error));
 
 //get all student details to dashboard
 let allStudentDts = localStorage.getItem("allStudents");
